ab88c6289cc29831f053aee5372eecd37f18bad8
thenetbeangang <Caorey13@hotmail.com>
2024-06-06 11:15:26 +0200

    Uncomment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,20 +10,20 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    //
-    // try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
-    //   while(true) {
-    //     final byte[] buf = new byte[512];
-    //     final DatagramPacket packet = new DatagramPacket(buf, buf.length);
-    //     serverSocket.receive(packet);
-    //     System.out.println("Received data");
-    //
-    //     final byte[] bufResponse = new byte[512];
-    //     final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
-    //     serverSocket.send(packetResponse);
-    //   }
-    // } catch (IOException e) {
-    //     System.out.println("IOException: " + e.getMessage());
-    // }
+    
+     try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
+       while(true) {
+         final byte[] buf = new byte[512];
+         final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+         serverSocket.receive(packet);
+         System.out.println("Received data");
+    
+         final byte[] bufResponse = new byte[512];
+         final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
+         serverSocket.send(packetResponse);
+       }
+     } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
+     }
   }
 }
